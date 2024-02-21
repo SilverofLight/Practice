ea93072060ed3492d56556ebaeda703baee3d9ca
Jiashuo_Li <a2061686759@outlook.com>
2024-02-21 14:18:56 +0800

    第一次不用改bug作对一道题
    第一次不用改bug作对一道题

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
   [接雨水](array/trap/main.go)  
   [旋转图像](array/rotate/main.go)  
   [字母异位词分组](array/groupAnagrams/main.go)
+  [螺旋矩阵](array/spiralOrder/main.go)
 
 ## dfs
   [平衡树](dfs/balanceTree/main.go)  
--- array/spiralOrder/main.go
+++ array/spiralOrder/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
 示例 1：
 
@@ -23,6 +25,43 @@ func spiralOrder(matrix [][]int) []int {
 	m := len(matrix)
 	n := len(matrix[0])
 	cil := make([]int, m*n)
-	
-	if 
+	count := 0
+
+re:
+	for i := 0; ; i++ {
+		for j := i; j < n-i; j++ {
+			cil[count] = matrix[i][j]
+			count++
+			if count == m*n {
+				break re
+			}
+		}
+		for j := i + 1; j < m-i; j++ {
+			cil[count] = matrix[j][n-i-1]
+			count++
+			if count == m*n {
+				break re
+			}
+		}
+		for j := n - i - 2; j >= i; j-- {
+			cil[count] = matrix[m-1-i][j]
+			count++
+			if count == m*n {
+				break re
+			}
+		}
+		for j := m - i - 2; j > i; j-- {
+			cil[count] = matrix[j][i]
+			count++
+			if count == m*n {
+				break re
+			}
+		}
+	}
+	return cil
+}
+
+func main() {
+	matrix := [][]int{{1, 1, 2}, {1, 1, 2}}
+	fmt.Println(spiralOrder(matrix))
 }
