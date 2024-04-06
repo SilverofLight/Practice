893e570a5f3a0ce4fcb5d9fac818e8a2c850adc5
Jiashuo_Li <a2061686759@outlook.com>
2024-04-06 21:27:18 +0800

    Update App.js

--- frontEnd/firstapp/src/App.js
+++ frontEnd/firstapp/src/App.js
@@ -1,39 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-const type = {
-  backgroundColor: "lightblue",
-  borderColor: "black",
-  borderRadius: "5px",
-  margin: "3px",
-  padding: "10px",
-
-}
-
-const displays = {
-  display: "grid",
-  gridTemplateAreas: "one two three",
-  gridTemplateColumns: "1fr 1fr 1fr",
-}
-
-function Card(props) {
-  return(
-    <div className='card' style={type}>
-      <h2>{props.h2}</h2>
-      <h3>{props.h3}</h3>
-    </div>
-  );
-}
 
 function App() {
-  return(
-    <div className='App'>
-      <h1>This is a Text of Card</h1>
-      <div style={displays}>
-        <Card h2="First Card's h2" h3="First Card's h3"/>
-        <Card h2="Second Card's h2" h3="Second Card's h3"/>
-        <Card h2="Third Card's h2" h3="Third Card's h3"/>
-      </div>
+  function handleClick() {
+    let computerNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(computerNumber);
+    let userNumber = prompt("type a number");
+    alert(`Computer number is ${computerNumber}, you guess ${userNumber}`);
+  }
+
+  return (
+    <div>
+      <h1>Add a button and handle a click event</h1>
+      <button onClick={handleClick}>guess a number from 1~3</button>
     </div>
   )
 }
